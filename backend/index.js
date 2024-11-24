6191b6803cb57cce8c1a9fe2255fcf2ec72e93cc
Meowstro33 <mahdi.ibrahim.ph@gmail.com>
2024-11-24 14:07:16 +0200

    fix-node-modules

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const corsOptions = {
+    origin: 'http://localhost:3000',
+  };
+  
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
