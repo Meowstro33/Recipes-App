f8398779bdc25f0d08344ab6992246839daa73a0
Meowstro33 <mahdi.ibrahim.ph@gmail.com>
2024-11-25 16:15:38 +0200

    install:express-and-cors-and-create-dotenv

--- backend/index.js
+++ backend/index.js
@@ -5,16 +5,16 @@ require('dotenv').config();
 const app = express();
 const corsOptions = {
     origin: 'http://localhost:3000',
-  };
-  
+};
+
 app.use(cors(corsOptions));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Backend is running');
+    res.send('Backend is running');
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
